d14d5091bc2e27400d4dc942780f06f3967560be
Rohit Vishwakarma <Rohit993583@gmail.com>
2022-11-08 21:32:33 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,27 @@
 import React from "react";
-
+import { tmp2 } from "./App";
 class Movie extends React.Component {
   render() {
-
     // If no movie is selected, show message - "Invalid Id".
-    
+
     return (
-      <div id="movie-banner">
-        <h2>Movie title</h2>
-        <h2>Movie year</h2>
-        <h2>Movie director</h2>
-      </div>
+      <tmp2.Consumer>
+        {(item) => {
+          return (
+            <div id="movie-banner">
+              {JSON.stringify(item) === "{}" ? (
+                "Invalid Id"
+              ) : (
+                <>
+                  <h2>{item.title}</h2>
+                  <h2>{item.year}</h2>
+                  <h2>{item.director}</h2>
+                </>
+              )}
+            </div>
+          );
+        }}
+      </tmp2.Consumer>
     );
   }
 }
